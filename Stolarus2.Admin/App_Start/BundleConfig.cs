6da0dbbd1fc222dc8e88e7323a509612d1c2e696
Mark Vlasenko <mvlasenko@sdl.com>
2018-03-26 01:04:01 +0300

    file drop implemented

--- Stolarus2.Admin/App_Start/BundleConfig.cs
+++ Stolarus2.Admin/App_Start/BundleConfig.cs
@@ -12,34 +12,37 @@ namespace Stolarus2.Admin
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/datetimepicker").Include(
-                      "~/Scripts/moment-with-locales.js",
-                      "~/Scripts/bootstrap-datetimepicker.js"
-                      ));
+                "~/Scripts/moment-with-locales.js",
+                "~/Scripts/bootstrap-datetimepicker.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dropzone").Include(
+                "~/Scripts/dropzone/dropzone.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/ckeditor").Include(
-                      "~/Scripts/ckeditor/ckeditor.js"
-                      ));
+                "~/Scripts/ckeditor/ckeditor.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"
-                      ));
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/datetimepicker").Include(
-                "~/Content/bootstrap-datetimepicker.css"
-                ));
+                "~/Content/bootstrap-datetimepicker.css"));
+
+            bundles.Add(new StyleBundle("~/Content/dropzone").Include(
+                "~/Scripts/dropzone/dropzone.css"));
+
         }
     }
 }
--- Stolarus2.Admin/Controllers/SlidersController.cs
+++ Stolarus2.Admin/Controllers/SlidersController.cs
@@ -1,5 +1,9 @@
 using Stolarus2.Data.Contracts;
 using Stolarus2.Data.Models;
+using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Stolarus2.Admin.Controllers
 {
@@ -10,6 +14,36 @@ namespace Stolarus2.Admin.Controllers
         public SlidersController(ISlidersRepository repository) : base(repository)
         {
             this.repository = repository;
+        }
+
+        [HttpPost]
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            try
+            {
+                var memStream = new MemoryStream();
+                file.InputStream.CopyTo(memStream);
+
+                byte[] fileData = memStream.ToArray();
+
+                //save file to database using fictitious repository
+                //var repo = new FictitiousRepository();
+                //repo.SaveFile(file.FileName, fileData);
+            }
+            catch (Exception exception)
+            {
+                return Json(new
+                {
+                    success = false,
+                    response = exception.Message
+                });
+            }
+
+            return Json(new
+            {
+                success = true,
+                response = "File uploaded."
+            });
         }
     }
 }
